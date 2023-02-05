8153d55b3d1068a38347cf2ea3646b9554887cd9
phthai1 <thai.phamhuu1311@gmail.com>
2023-02-05 22:30:58 +0700

    fix some details

--- src/app/pages/post-and-story/post-and-story.module.ts
+++ src/app/pages/post-and-story/post-and-story.module.ts
@@ -1,25 +1,26 @@
-import {NgModule} from "@angular/core";
-import {PostAndStoryComponent} from "./post-and-story.component";
-import {CommonModule} from "@angular/common";
-import {PostAndStoryRoutingModule} from "./post-and-story.routing.module";
-import {NzRadioModule} from "ng-zorro-antd/radio";
-import {FormsModule} from "@angular/forms";
-import {NzButtonModule} from "ng-zorro-antd/button";
-import {NzGridModule} from "ng-zorro-antd/grid";
-import {NzSelectModule} from "ng-zorro-antd/select";
-import {NzTagModule} from "ng-zorro-antd/tag";
-import {NzIconModule} from "ng-zorro-antd/icon";
-import {NzTableModule} from "ng-zorro-antd/table";
+import { NgModule } from '@angular/core';
+import { PostAndStoryComponent } from './post-and-story.component';
+import { CommonModule } from '@angular/common';
+import { PostAndStoryRoutingModule } from './post-and-story.routing.module';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { FormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    PostAndStoryComponent
-  ],
+  declarations: [PostAndStoryComponent],
   imports: [
     CommonModule,
     PostAndStoryRoutingModule,
+    SharedModule,
     NzRadioModule,
     FormsModule,
     NzButtonModule,
@@ -29,7 +30,8 @@ import { NzProgressModule } from 'ng-zorro-antd/progress';
     NzIconModule,
     NzTableModule,
     NzInputModule,
-    NzProgressModule
-  ]
+    NzProgressModule,
+    NzAvatarModule,
+  ],
 })
-export class PostAndStoryModule { }
+export class PostAndStoryModule {}
